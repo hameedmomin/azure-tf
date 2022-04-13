27b28e6e53c919892db77063ceeff91f872f10db
USER <hameed.momin@gmail.com>
2022-04-13 19:45:33 +0530

    azure_project-1

--- first_file/provider.tf
+++ first_file/provider.tf
@@ -1,13 +1,19 @@
-resource "azurerm_resource_group" "first" {
-    name = my-resource
-    location = eastus
-
-  
+terraform {
+    required_version = ">= 1.0.0"
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "=3.0.0"
+        }
+    }
 }
 
 provider "azurerm" {
     features {}
-  
 }
 
-
+resource "random_string" "random" {
+        length           = 16
+        special          = false
+        override_special = "/@£$"
+}
---
+++ first_file/variables.tf
@@ -0,0 +1,6 @@
+variable "Environment" {
+  description = "Environment name"
+  default = "dev"
+  type = "string"
+
+}
