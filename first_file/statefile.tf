f2175b35c17c5ef80825f64e93affc80263fe5e9
USER <hameed.momin@gmail.com>
2022-04-14 10:33:42 +0530

    azure_project-1

--- first_file/statefile.tf
+++ first_file/statefile.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "${azurerm_resource_group.project.name}"
+    resource_group_name  = "azurerm_resource_group.project.name"
     storage_account_name = "new-project"
     container_name       = "project1"
     key                  = "terraform.tfstate"
