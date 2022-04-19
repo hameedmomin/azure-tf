3899fa4c38f99bd68fae75ae7b535927249afe51
USER <hameed.momin@gmail.com>
2022-04-19 16:50:46 +0530

    project

--- project-1/storage-account.tf
+++ project-1/storage-account.tf
@@ -1,11 +1,11 @@
 resource "random_string" "local" {
   length                   = 16
   upper                    = false
-  special                  = false
+  special                  = true
 }
 resource "azurerm_storage_account" "example" {
   depends_on               = [azurerm_resource_group.dev]
-  name                     = "my${random_string.local.id}"
+  name                     = "my-${random_string.local.id}"
   resource_group_name      = azurerm_resource_group.dev.name
   location                 = azurerm_resource_group.dev.location
   account_tier             = "Standard"
