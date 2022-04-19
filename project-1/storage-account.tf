fcd2763254031a65274d846127fd71743e90800d
USER <hameed.momin@gmail.com>
2022-04-19 16:40:06 +0530

    project

---
+++ project-1/storage-account.tf
@@ -0,0 +1,13 @@
+resource "random_string" "local" {
+  length                   = 16
+  upper                    = false
+  special                  = false
+}
+resource "azurerm_storage_account" "example" {
+  name                     = "my-${random_string.local.id}"
+  resource_group_name      = azurerm_resource_group.dev.name
+  location                 = azurerm_resource_group.dev.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
