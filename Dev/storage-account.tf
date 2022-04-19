7df9a9bb6988c483afb9b4565c238294e1d8a483
USER <hameed.momin@gmail.com>
2022-04-19 11:07:34 +0530

    my learning

--- Dev/storage-account.tf
+++ Dev/storage-account.tf
@@ -1,13 +1,13 @@
-/*resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "example" {
   name                     = "hameed123"
   resource_group_name      = azurerm_resource_group.dev.name
   location                 = azurerm_resource_group.dev.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
-  *//*  tags = {
+    tags = {
     environment = "staging"
-  }*//*
+  }
 }
 
 terraform {
@@ -18,4 +18,4 @@ terraform {
     key                  = "terraform.tfstate"
   }
 
-}*/
+}
