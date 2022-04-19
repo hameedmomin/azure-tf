22b16efe003f9269e4f11b070bec503f647076f5
USER <hameed.momin@gmail.com>
2022-04-19 11:01:42 +0530

    my learning

--- Dev/main.tf
+++ Dev/main.tf
@@ -6,25 +6,15 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "terraform-Dev"
-    storage_account_name = "hameed123"
-    container_name       = "tfstatefiles"
-    key                  = "terraform.tfstate"
-  }
 }
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-  features {}
-}
-
 # Create a resource group
 resource "azurerm_resource_group" "dev" {
   name                   = "Dev"
   location               = "East US"
 }
 
-terraform {
 
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 }
--- Dev/storage-account.tf
+++ Dev/storage-account.tf
@@ -1,11 +1,21 @@
 resource "azurerm_storage_account" "example" {
-  name                                = "hameed123"
-  resource_group_name                 = azurerm_resource_group.dev.name
-  location                            = azurerm_resource_group.dev.location
-  account_tier                        = "Standard"
-  account_replication_type            = "GRS"
+  name                     = "hameed123"
+  resource_group_name      = azurerm_resource_group.dev.name
+  location                 = azurerm_resource_group.dev.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
-/*  tags = {
+  /*  tags = {
     environment = "staging"
   }*/
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "${azurerm_resource_group.dev.name}"
+    storage_account_name = "${azurerm_storage_account.example.name}"
+    container_name       = "tfstatefiles"
+    key                  = "terraform.tfstate"
+  }
+
 }
