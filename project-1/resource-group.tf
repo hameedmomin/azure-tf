560b5d97936a3151d2efed10bf189ec8ea4f2b64
USER <hameed.momin@gmail.com>
2022-04-19 15:12:09 +0530

    project

--- project-1/resource-group.tf
+++ project-1/resource-group.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "dev" {
-  provider            = "azurerm.dev"
+  provider            = azurerm.dev
   location            = "East US"
   name                = "dev-rgroup"
 }
 
 resource "azurerm_resource_group" "pro" {
-  provider            = "azurerm.pro"
+  provider            = azurerm.pro
   location            = "West US"
   name                = "pro-rgroup"
 }
