8a43672fbcd2eea83c6358ad304a7996447cefe8
USER <hameed.momin@gmail.com>
2022-04-19 16:45:16 +0530

    project

--- project-1/storage-account.tf
+++ project-1/storage-account.tf
@@ -1,7 +1,7 @@
 resource "random_string" "local" {
   length                   = 16
   upper                    = false
-  special                  = false
+  special                  = ture
 }
 resource "azurerm_storage_account" "example" {
   depends_on               = [azurerm_resource_group.dev]
