e2252db3e2ddba2a9cba3e888916a076b4a248f7
USER <hameed.momin@gmail.com>
2022-04-19 10:47:29 +0530

    my learning

--- Dev/vars.tf
+++ Dev/vars.tf
@@ -1,43 +1,24 @@
 variable "rg-name" {
-<<<<<<< HEAD
   description = "Resource Group Name"
   default     = "project"
   type        = string
 }
 
 variable "environment" {
-  default     = "dev"
   description = "Environment name"
+  default     = "dev"
   type        = string
 }
 
 variable "rg-location" {
-  default     = "east us"
   description = "Resource location"
+  default     = "east us"
   type        = string
 }
-=======
-  description            = "Resource Group Name"
-  default                = "project"
-  type                   = string
-}
 
-variable "environment" {
-  default                 = "dev"
-  description             = "Environment name"
-  type                    = string
-}
-
-variable "rg-location" {
-  default                 = "east us"
-  description             = "Resource location"
-  type                    = string
-}
 variable "backend" {
   default                 = "dev-storage-account"
   description             = "Storage Account Name"
   type                    = string
 
 }
-#variable "storage account name" {}
->>>>>>> 92b481923a9e0c259af1cb3f1513a0af72cbe4a3
