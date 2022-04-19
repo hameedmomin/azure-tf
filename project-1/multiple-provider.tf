c6122f9f6272c93b1c0e816bedd6326a4f5bf365
USER <hameed.momin@gmail.com>
2022-04-19 15:35:16 +0530

    project

--- project-1/multiple-provider.tf
+++ project-1/multiple-provider.tf
@@ -9,7 +9,11 @@ terraform {
 
 provider "azurerm" {
   alias          = "dev"
-  features {}
+  features {
+    virtual_machine {
+      delete_os_disk_on_deletion = false # This will ensure when the Virtual Machine is destroyed, Disk is not deleted, default is true and we can alter it at provider level
+    }
+  }
 }
 
 provider "azurerm" {
