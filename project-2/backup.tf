090d451700b751896b7584b9b9d791809f38b192
USER <hameed.momin@gmail.com>
2022-04-20 15:37:11 +0530

    project2

--- project-2/backup.tf
+++ project-2/backup.tf
@@ -1,10 +1,12 @@
+/*
 terraform {
   backend "azurerm" {
 
     resource_group_name                        = "azurerm_resource_group.project2.name"
     storage_account_name                       = "azurerm_storage_account.project2.name"
-    container_name                             = "azurerm_storage_container.storage.name"
+    container_name                             = "terraform"
     key                                        = "dev.terraform.tfstate"
 
   }
 }
+*/
--- project-2/storage.tf
+++ project-2/storage.tf
@@ -7,9 +7,9 @@ resource "azurerm_storage_account" "project2" {
   resource_group_name                          = azurerm_resource_group.project2.name
 }
 
-resource "azurerm_storage_container" "Storage" {
+resource "azurerm_storage_container" "storage" {
   depends_on                                   = [azurerm_storage_account.project2]
-  name                                         = "${var.ENV}-storage"
+  name                                         = "terraform"
   storage_account_name                         = azurerm_storage_account.project2.name
   container_access_type                        = "private"
 }
